bb6417ce9d923f450e4e6cacaed39b844d21fb05
Ilya Kolmakov <ilkolmakov@yandex.ru>
2024-01-17 15:30:56 +0500

    feat: module3-task1 connect mongodb

--- project/apps/user/src/app/app.module.ts
+++ project/apps/user/src/app/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { BlogUserModule } from './blog-user/blog-user.module';
 import { AuthenticationModule } from './authentication/authentication.module';
-
+import { ConfigUserModule, getMongooseOptions } from '@project/libs/shared/config/user';
 @Module({
-  imports: [BlogUserModule, AuthenticationModule],
+  imports: [
+    BlogUserModule, 
+    AuthenticationModule, 
+    ConfigUserModule,
+    MongooseModule.forRootAsync(
+      getMongooseOptions()
+    )
+  ],
   controllers: [],
   providers: [],
 })
---
+++ project/libs/shared/config/user/src/lib/mongodb/get-mongoose-options.ts
@@ -0,0 +1,32 @@
+import { getMongoConnectionString } from '@project/libs/shared/helpers';
+import { MongooseModuleAsyncOptions } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+
+// INFO: Используем фабричную функцию для внедрения зависимости getMongooseOptions
+export function getMongooseOptions(): MongooseModuleAsyncOptions {
+  return {
+    useFactory: async (config: ConfigService) => {
+      console.log('123', 
+      getMongoConnectionString({
+        username: config.get<string>('db.user') || "",
+        password: config.get<string>('db.password') || "",
+        host: config.get<string>('db.host') || "",
+        port: config.get<string>('db.port') || "",
+        authDatabase: config.get<string>('db.authBase') || "",
+        databaseName: config.get<string>('db.name') || "",
+      })
+      );
+      return {
+        uri: getMongoConnectionString({
+          username: config.get<string>('db.user') || "",
+          password: config.get<string>('db.password') || "",
+          host: config.get<string>('db.host') || "",
+          port: config.get<string>('db.port') || "",
+          authDatabase: config.get<string>('db.authBase') || "",
+          databaseName: config.get<string>('db.name') || "",
+        })
+    }
+  },
+  inject: [ConfigService]
+}
+}
